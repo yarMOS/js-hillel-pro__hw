d4065b1cdcbf3e85d4291c8a48aae91002b70722
Yaroslav Mospan <mossspan@gmail.com>
2025-06-12 15:12:20 +0200

    переробив

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-'use strickt';
+'use strict'
 
-let input = prompt("Введіть п'ятизначне число:");
+let userAge = prompt("Ваше 5-ти значне число?");
 
-if (/^\d{5}$/.test(input)) {
-    let result = input.split('').join(' ');
-    alert(result);
-} 
-else {
-    alert('Помилка: потрібно ввести п’ятизначне число');
+if (userAge === null || userAge.trim() === "") {
+    alert("Шкода, що Ви не захотіли вказати Ваше 5-ти значне число.");
+    userAge = "Користувач не вказав 5-ти значне число";
+} else if (isNaN(userAge)) {
+    alert("Ви ввели некоректне значення! Введіть Ваше 5-ти значне число у числовому вигляді.");
+    userAge = "Користувач не вказав число";
+} else {
+    userAge = +userAge;
+    if (userAge <= 10000 || userAge >= 99999) {
+        alert("Будь ласка, введіть 5-ти значне число .");
+        userAge = "Користувач не вказав 5-ти значне число";
+    } else {
+        let space = String(userAge).split("").join(" ");
+        alert(`Ваше 5-ти значне число : ${space}`);
+    }
 }
