621a346932c424e12c6aa8c2c167313c1d24b6fb
Yaroslav Mospan <mossspan@gmail.com>
2025-06-14 11:44:42 +0200

    second commit

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
 'use strict';
 
 alert(`Введіть будь ласка ваші данні:`);
-const userName = prompt('Ваше імʼя ?');
-    if (userName === null || userName === '') {
-        alert(`Шкода, що Ви не захотіли вказати своє імʼя.`);
-        userName = `Користувач не вказав імʼя.`;
-    } else if(!isNaN(userName)){
-        alert(`Ви ввели некоректне значення! Введіть ваше імʼя не в числовому значенні.`);
-        userName = prompt('Ваше імʼя ?');
-    }
 
-const userAge = +prompt(`Ваш вік ?`);
-    if (userAge === 0 || userAge === '') {
-        alert(`Шкода, що Ви не захотіли вказати свій вік.`);
-        userAge = `Користувач не вказав кількість`;
-    } else  if(isNaN(userAge)) {
-        alert(`Ви ввели некоректне значення! Введіть ваш вік в числовому значенні.`);
-        userAge = +prompt(`Ваш вік ?`);
+let userAge = prompt('Ваш вік:');
+if (userAge === null || userAge === ''){
+    alert('Шкода, що Ви не захотіли вказати свій вік.');
+    userAge = 'Користувач не вказав кількість.'
+} else if (isNaN(userAge)){
+    alert("Ви ввели некоректне значення! Введіть ваш вік в числовому значенні.");
+    userAge = +prompt('Ваш вік:');
+    if (isNaN(userAge) || userAge === null || userAge === ''){
+        alert('Шкода, що Ви не захотіли вказати свій вік.');
+        userAge = 'Користувач не вказав кількість.';
     }
+}
 
-const userJob = prompt(`Ваше місто роботи ?`);
-    if (userJob === null || userJob === '') {
-        alert(`Шкода, що Ви не захотіли вказати своє місце роботи.`);
-        userJob = `Користувач не вказав місце роботи.`;
-    } else if(!isNaN(userJob)){
-        alert(`Ви ввели некоректне значення! Введіть ваше місце не в числовому значенні.`);
-        userJob = prompt(`Ваше місце роботи ?`);
+let userCity = prompt('Ваше місце проживання:');
+if (userCity === null || userCity === ''){
+    alert('Шкода, що Ви не захотіли вказати своє місце проживання.');
+    userCity = 'Користувач не вказав своє місце проживання.'
+} else if (!isNaN(userCity)){
+    alert("Введіть ваше місце проживання не в числовому значенні");
+    userCity = 'Користувач вказав числове значення';
+    if (!isNaN(userCity) || userCity === null || userCity === ''){
+        alert('Шкода, що не захотіли ввести своє місце проживання');
+        userCity = 'Користувач не захотів вводити свої дані';
     }
+}
 
-alert(`Імʼя користувача: ${userName}, Вік користувача: ${userAge}, Місце користувача: ${userJob}`)
+let userFavorSport = prompt('Ваш улюблений вид спорту:');
+if (userFavorSport === null || userFavorSport === ''){
+    alert('Шкода, що Ви не захотіли вказати улюблений вид спорту.');
+    userName = 'Користувач не вказав улюблений вид спорту.'
+} else if (!isNaN(userFavorSport)){
+    alert("Введіть ваш улюблений вид спорту не в числовому значенні");
+    userFavorSport = 'Користувач вказав числове значення';
+    if (!isNaN(userFavorSport) || userFavorSport === null || userFavorSport === ''){
+        alert('Шкода, що не захотіли вказати улюблений вид спорту.');
+        userFavorSport = 'Користувач не захотів вводити свої дані';
+    }
+}
+
+alert(`Імʼя користувача: ${userAge}, Вік користувача: ${userCity}, Місце користувача: ${userFavorSport}`)
