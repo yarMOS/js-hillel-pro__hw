9a3aefa2406ea46bc1f4c59dbcd6a9cf114152b7
Yaroslav Mospan <mossspan@gmail.com>
2025-06-16 15:20:04 +0200

    .trim()

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 alert(`Введіть будь ласка ваші данні:`);
 
 let userAge = prompt('Ваш вік:');
-if (userAge === null || userAge === ''){
+if (userAge === null || userAge.trim() === ''){
     alert('Шкода, що Ви не захотіли вказати свій вік.');
     userAge = 'Користувач не вказав кількість.'
 } else if (isNaN(userAge)){
@@ -16,28 +16,22 @@ if (userAge === null || userAge === ''){
 }
 
 let userCity = prompt('Ваше місце проживання:');
-if (userCity === null || userCity === ''){
+if (userCity === null || userCity.trim() === ''){
     alert('Шкода, що Ви не захотіли вказати своє місце проживання.');
     userCity = 'Користувач не вказав своє місце проживання.'
-} else if (!isNaN(userCity)){
+    if (!isNaN(userCity)){
     alert("Введіть ваше місце проживання не в числовому значенні");
     userCity = 'Користувач вказав числове значення';
-    if (!isNaN(userCity) || userCity === null || userCity === ''){
-        alert('Шкода, що не захотіли ввести своє місце проживання');
-        userCity = 'Користувач не захотів вводити свої дані';
-    }
+    } 
 }
 
 let userFavorSport = prompt('Ваш улюблений вид спорту:');
-if (userFavorSport === null || userFavorSport === ''){
+if (userFavorSport === null || userFavorSport.trim() === ''){
     alert('Шкода, що Ви не захотіли вказати улюблений вид спорту.');
     userName = 'Користувач не вказав улюблений вид спорту.'
-} else if (!isNaN(userFavorSport)){
+    if (!isNaN(userFavorSport)){
     alert("Введіть ваш улюблений вид спорту не в числовому значенні");
     userFavorSport = 'Користувач вказав числове значення';
-    if (!isNaN(userFavorSport) || userFavorSport === null || userFavorSport === ''){
-        alert('Шкода, що не захотіли вказати улюблений вид спорту.');
-        userFavorSport = 'Користувач не захотів вводити свої дані';
     }
 }
 
